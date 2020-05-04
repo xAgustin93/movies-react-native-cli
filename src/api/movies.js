7e1cabd5493294b6af2b33d3e54f39adb2544d1f
Agustin Navarro <xAgustin93@gmail.com>
2020-05-04 16:15:23 +0200

    New movies

--- src/api/movies.js
+++ src/api/movies.js
@@ -1,7 +1,7 @@
 import {API_HOST, API_KEY, LANG} from '../utils/constants';
 
-export function getNewsMoviesApi() {
-  const url = `${API_HOST}/movie/now_playing?api_key=${API_KEY}&language=${LANG}`;
+export function getNewsMoviesApi(page = 1) {
+  const url = `${API_HOST}/movie/now_playing?api_key=${API_KEY}&language=${LANG}&page=${page}`;
 
   return fetch(url)
     .then((response) => {
--- src/screens/News.js
+++ src/screens/News.js
@@ -1,12 +1,108 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {
+  StyleSheet,
+  View,
+  ScrollView,
+  Dimensions,
+  Image,
+  TouchableWithoutFeedback,
+} from 'react-native';
+import {Button, Text} from 'react-native-paper';
+import {map} from 'lodash';
+import {getNewsMoviesApi} from '../api/movies';
+import {BASE_PATH_IMG} from '../utils/constants';
+import usePreferences from '../hooks/usePreferences';
 
-export default function News() {
+const {width} = Dimensions.get('window');
+
+export default function News(props) {
+  const {navigation} = props;
+  const [movies, setMovies] = useState(null);
+  const [page, setPage] = useState(1);
+  const [showBtnMore, setshowBtnMore] = useState(true);
+  const {theme} = usePreferences();
+
+  useEffect(() => {
+    getNewsMoviesApi(page).then((response) => {
+      const totlaPages = response.total_pages;
+      if (page < totlaPages) {
+        if (!movies) {
+          setMovies(response.results);
+        } else {
+          setMovies([...movies, ...response.results]);
+        }
+      } else {
+        setshowBtnMore(false);
+      }
+    });
+  }, [page]);
+
   return (
-    <View>
-      <Text>News</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        {map(movies, (movie, index) => (
+          <Movie key={index} movie={movie} navigation={navigation} />
+        ))}
+      </View>
+      {showBtnMore && (
+        <Button
+          mode="contained"
+          contentStyle={styles.loadMoreContainer}
+          style={styles.loadMore}
+          labelStyle={{color: theme === 'dark' ? '#fff' : '#000'}}
+          onPress={() => setPage(page + 1)}>
+          Cargar mas...
+        </Button>
+      )}
+    </ScrollView>
   );
 }
 
-const styles = StyleSheet.create({});
+function Movie(props) {
+  const {movie, navigation} = props;
+  const {id, poster_path, title} = movie;
+
+  const goMovie = () => {
+    navigation.navigate('movie', {id});
+  };
+
+  return (
+    <TouchableWithoutFeedback onPress={goMovie}>
+      <View style={styles.movie}>
+        {poster_path ? (
+          <Image
+            source={{uri: `${BASE_PATH_IMG}/w500${poster_path}`}}
+            style={styles.image}
+          />
+        ) : (
+          <Text>{title}</Text>
+        )}
+      </View>
+    </TouchableWithoutFeedback>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  movie: {
+    width: width / 2,
+    height: 300,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+  loadMoreContainer: {
+    paddingTop: 10,
+    paddingBottom: 30,
+  },
+  loadMore: {
+    backgroundColor: 'transparent',
+  },
+});
